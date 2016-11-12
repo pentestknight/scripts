c8cc7776564e143d99fe2fd90f072032496e15af
PentestKnight <g0td4un1k@gmail.com>
2016-11-13 00:22:17 +1100

    Add files via upload

--- reconscan.sh
+++ reconscan.sh
@@ -2,9 +2,10 @@
 # clearing the screen before execution
 clear
 echo
-echo -e "\e[1;34m$(date)\e[0m"
+echo -e "\e[1;34m$(date)\e[0m" # Display date [ 31=red 32=green 33=yellow 34=blue 35=magenta 36=cyan]
 echo
 echo
+# Here goes the branding
 echo "[---------------------------------------]"
 echo -e [---------"\e[1;33mAuthor:\e[0m""\e[1;32mG0tD4un1k\e[0m" -------------]
 echo -e [---------"\e[1;36mR E C O N - S C A N\e[0m"-----------]
@@ -14,9 +15,11 @@ echo "[----------------[V0.1]-----------------]"
 echo "[---------------------------------------]"
 echo
 echo
+# create a directory to store the results
+mkdir -p /$(whoami)/reconscan
+touch /$(whoami)/reconscan/target_list.txt
+# Collect user input
 read -p "Enter an IP / hostname to scan : " tip
-echo
-#echo -e "[+]" Using "\e[1;36m"$tip"\e[0m" as target For RECON SCAN
 echo
 echo
 if [ -z $tip ]; then
@@ -24,7 +27,11 @@ echo
 echo
 echo -e "\e[1;31mNo Input provided "[Aborting...Restart the program and provide input]"\e[0m"
 else
-echo -e "[+]" Using "\e[1;36m"$tip"\e[0m" as target For RECON SCAN
+echo -e "[+]" Using "\e[1;36;4m"$tip"\e[0m" as target For RECON SCAN
+echo >> "/$(whoami)/reconscan/target_list.txt"
+echo "[+]" "[---------------------New Scan------------------]" >> "/$(whoami)/reconscan/target_list.txt"
+date >> "/$(whoami)/reconscan/target_list.txt"
+echo "The selected target: " $tip >> "/$(whoami)/reconscan/target_list.txt"
 echo
 fi
 echo
@@ -35,8 +42,18 @@ select opt in "${options[@]}"
 do
     case $opt in
         "Regular Scan")
-            echo "you chose choice 1"
-	    nmap $tip > nmap_regularscanresults.txt
+            echo 
+	    echo -e "[==>]" "\e[34mThis will perform a regular nmap scan with predetermined parameters.\e[0m"
+	    echo
+            read -p "Press enter to continue"
+            echo "Please wait...scan in progree and may take some time"
+            echo >> "/$(whoami)/reconscan/nmap_regularscan.txt"
+            echo >> "[+]" "[------------Scan Results------------]" >> "/$(whoami)/reconscan/nmap_regularscan.txt"
+            date >> "/$(whoami)/reconscan/nmap_regularscan.txt" 
+            nmap -Pn $tip >> "/$(whoami)/reconscan/nmap_regularscan.txt"
+            echo "Scan complete - Check results"
+            echo
+            echo
             ;;
         "Quick Scan")
             echo "you chose choice 2"
