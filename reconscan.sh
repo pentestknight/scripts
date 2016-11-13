29c30adf5baeff8caf1af9b2e51536760153f452
PentestKnight <g0td4un1k@gmail.com>
2016-11-13 23:13:30 +1100

    Add files via upload

--- reconscan.sh
+++ reconscan.sh
@@ -37,18 +37,18 @@ fi
 echo
 echo
 PS3='Select the scan type: '
-options=("Regular Scan" "Quick Scan" "Intense Scan" "All TCP ports" "Slow & Comprehensive scan" "Top ports" "Quit")
+options=("Regular Scan" "Quick Scan" "Intense Scan" "All TCP ports" "Slow & Comprehensive scan" "Top ports" "Specific Ports" "Quit")
 select opt in "${options[@]}"
 do
     case $opt in
         "Regular Scan")
             echo 
-	    echo -e "[==>]" "\e[34mThis will perform a regular nmap scan with predetermined parameters.\e[0m"
+	    echo -e "[==>]" "\e[34mThis will perform a 'regular' nmap scan with predetermined parameters.\e[0m"
 	    echo
             read -p "Press enter to continue"
-            echo "Please wait...scan in progree and may take some time"
+            echo "Please wait...scan in progress and may take some time"
             echo >> "/$(whoami)/reconscan/nmap_regularscan.txt"
-            echo >> "[+]" "[------------Scan Results------------]" >> "/$(whoami)/reconscan/nmap_regularscan.txt"
+            echo "[+]" "[------------Scan Results------------]" >> "/$(whoami)/reconscan/nmap_regularscan.txt"
             date >> "/$(whoami)/reconscan/nmap_regularscan.txt" 
             nmap -Pn $tip >> "/$(whoami)/reconscan/nmap_regularscan.txt"
             echo "Scan complete - Check results"
@@ -56,25 +56,91 @@ do
             echo
             ;;
         "Quick Scan")
-            echo "you chose choice 2"
-	    nmap -T4 -F $tip > nmap_quickscanresults.txt
+            echo 
+	    echo -e "[==>]" "\e[34mThis will perform a 'quick' nmap scan with predetermined parameters.\e[0m"
+	    echo
+            read -p "Press enter to continue"
+            echo "Please wait...scan in progress and may take some time...should be quick though :)"
+            echo >> "/$(whoami)/reconscan/nmap_quickscan.txt"
+            echo "[+]" "[------------Scan Results------------]" >> "/$(whoami)/reconscan/nmap_quickscan.txt"
+            date >> "/$(whoami)/reconscan/nmap_quickscan.txt" 
+	    nmap -Pn -T4 -F $tip >> "/$(whoami)/reconscan/nmap_quickscan.txt" 
+	    echo "Scan complete - Check results"
+            echo
+            echo
             ;;
         "Intense Scan")
-            echo "you chose choice 3"
-	    nmap -T4 -v -A $tip > nmap_intensescanresults.txt
+            echo 
+	    echo -e "[==>]" "\e[34mThis will perform an 'intense' nmap scan with predetermined parameters.\e[0m"
+	    echo
+            read -p "Press enter to continue"
+            echo "Please wait...scan in progress and may take some time....."
+            echo >> "/$(whoami)/reconscan/nmap_intensescan.txt"
+            echo "[+]" "[------------Scan Results------------]" >> "/$(whoami)/reconscan/nmap_intensescan.txt"
+            date >> "/$(whoami)/reconscan/nmap_intensescan.txt" 
+	    nmap -Pn -T4 -v -A $tip >> "/$(whoami)/reconscan/nmap_intensescan.txt"
+	    echo "Scan complete - Check results"
+            echo
+            echo
             ;;
 	"All TCP ports")
-	    echo "you chose choice 4"
-	    nmap -Pn -p- -v -A -T4 $tip > nmap_alltcpportsscanresults.txt
+	    echo 
+	    echo -e "[==>]" "\e[34mThis will perform an nmap scan on 'all TCP ports' with predetermined parameters.\e[0m"
+	    echo
+            read -p "Press enter to continue"
+            echo "Please wait...scan in progress and may take some time....."
+            echo >> "/$(whoami)/reconscan/nmap_allTCPportsscan.txt"
+            echo "[+]" "[------------Scan Results------------]" >> "/$(whoami)/reconscan/nmap_allTCPportsscan.txt"
+            date >> "/$(whoami)/reconscan/nmap_allTCPportsscan.txt" 
+	    nmap -Pn -p- -v -A -T4 $tip >> "/$(whoami)/reconscan/nmap_allTCPportsscan.txt"
+	    echo "Scan complete - Check results"
+            echo
+            echo
 	    ;;
 	"Slow & Comprehensive scan")
-	    echo "you chose choice 5"
-	    nmap -sS -sU -T4 -A -v -PE -PP -PS80,443 -PA3389 -PU40125 -PY -g 53 --script "default or (discovery and safe)" $tip > nmap_slowcomprehensivescanresults.txt
+	    echo 
+	    echo -e "[==>]" "\e[34mThis will perform a 'Slow & Comprehensive' nmap scan with predetermined parameters.\e[0m"
+	    echo
+            read -p "Press enter to continue"
+            echo "Please wait...scan in progress and may take some time....."
+            echo >> "/$(whoami)/reconscan/nmap_comprehensivescan.txt"
+            echo "[+]" "[------------Scan Results------------]" >> "/$(whoami)/reconscan/nmap_comprehensivescan.txt"
+            date >> "/$(whoami)/reconscan/nmap_comprehensivescan.txt" 
+	    nmap -sS -sU -T4 -A -v -PE -PP -PS80,443 -PA3389 -PU40125 -PY -g 53 --script "default or (discovery and safe)" $tip >> "/$(whoami)/reconscan/nmap_comprehensivescan.txt"
+	    echo "Scan complete - Check results"
+            echo
+            echo
 	    ;;
 	"Top ports")
-	    echo "you chose choice 6"
-	    nmap -Pn -T4 -v -A --top-ports $tip > nmap_topportscanresults.txt
+	    echo 
+	    echo -e "[==>]" "\e[34mThis will perform an nmap scan on 'top ports'with predetermined parameters.\e[0m"
+	    echo
+	    read -p "Enter top ports to scan (e.g. 50): " tp
+            read -p "Press enter to continue"
+            echo "Please wait...scan in progress and may take some time....."
+            echo >> "/$(whoami)/reconscan/nmap_topportsscan.txt"
+            echo "[+]" "[------------Scan Results------------]" >> "/$(whoami)/reconscan/nmap_topportsscan.txt"
+            date >> "/$(whoami)/reconscan/nmap_topportsscan.txt" 
+	    nmap -Pn -T4 -v -A $tip --top-ports $tp >> "/$(whoami)/reconscan/nmap_topportsscan.txt"
+	    echo "Scan complete - Check results"
+            echo
+            echo
 	    ;;
+    "Specific Ports")
+        echo 
+        echo -e "[==>]" "\e[34mThis will perform an nmap scan on 'Specific Ports'with predetermined parameters.\e[0m"
+        echo
+        read -p "Enter ports to scan (e.g. 80,443,21,8080): " sp
+            read -p "Press enter to continue"
+            echo "Please wait...scan in progress and may take some time....."
+            echo >> "/$(whoami)/reconscan/nmap_spportsscan.txt"
+            echo "[+]" "[------------Scan Results------------]" >> "/$(whoami)/reconscan/nmap_spportsscan.txt"
+            date >> "/$(whoami)/reconscan/nmap_spportsscan.txt" 
+        nmap -Pn -T4 -v -p $sp -A $tip >> "/$(whoami)/reconscan/nmap_spportsscan.txt"
+        echo "Scan complete - Check results"
+            echo
+            echo
+        ;;    
         "Quit")
             break
             ;;
